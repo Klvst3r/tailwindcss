bf2ffc8b365867d5f182993ce243deb560c015d1
Klvst3r <klvst3r@gmail.com>
2025-11-03 14:11:33 -0600

    1 - Trabajo con columnas y filas en TalwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+    export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        container:{
+            center: true,
+        },
+        extend: {
+            gridTemplateColumns:{
+                //Simple 16 columns grid
+                '16' : 'repeat(16, minmax(0,1fr))',
+
+                //Complex site-specific column configration
+                'footer' : '200px minmax(900px, 1fr) 100px',
+            }
+        },
+    },
+    plugins: [],
+    };
