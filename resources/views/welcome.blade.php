3ce4cedb3bbfc07261cbe776e57bab4f57964bb7
Klvst3r <klvst3r@gmail.com>
2025-11-05 09:37:13 -0600

    Iniciando con padings y bordes en cajas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,13 @@
 </head>
 
 <body>
-    
+    <div class="container mt-4">
+        <div class="bg-gray-300 w-64 h-32 p-8 border-8 border-gray-500 box-content">
+            <div class="bg-gray-500 h-full w-full">
+                
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
