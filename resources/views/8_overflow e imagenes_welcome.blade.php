3d9d2df41caa54857786ea5206e4a51602e6cb52
Klvst3r <klvst3r@gmail.com>
2025-11-05 10:19:24 -0600

    Finaliza el tema de los flow

---
+++ resources/views/8_overflow e imagenes_welcome.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
+    <title>Tailwind CSS</title>
+
+    <!-- Se cargan los estilos Styles / Scripts de Tailwind-->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="bg-gray-400 p-4 h-64 overflow-auto">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe accusantium laudantium esse. Molestiae
+                architecto qui possimus voluptatibus. Optio incidunt voluptas tenetur qui error. Fuga dolore sit at
+                porro dolorum recusandae?</p>
+            <p>Deleniti repellendus consequatur aut repudiandae tempora facilis nesciunt nobis debitis cupiditate soluta
+                voluptates rem minima ipsum, expedita consequuntur aliquid quibusdam numquam maxime illum excepturi
+                dolores. Reiciendis tenetur deserunt minus cupiditate.</p>
+            <p>Saepe consequatur repellendus repudiandae minus asperiores minima odio odit mollitia explicabo illo, ipsa
+                quod eum ad rem eos ullam possimus tempore, soluta deserunt itaque quisquam non eius? Ullam, molestias
+                recusandae.</p>
+            <p>Cupiditate repellat ipsam voluptates adipisci unde doloremque, deserunt consequatur officiis maiores
+                nostrum quis, iusto sed corrupti itaque totam, asperiores eum deleniti cum consequuntur enim? Neque cum
+                laborum asperiores debitis pariatur!</p>
+            <p>Doloremque ratione natus quod magnam explicabo eveniet minus repellat, minima doloribus similique tempora
+                libero esse non id soluta saepe. Ipsam nobis doloribus in aut cumque velit cum explicabo ad magni?</p>
+            <p>Odio dolorum eius sequi deserunt ad provident vero iste asperiores ratione sit omnis corrupti facere amet
+                delectus, cupiditate eos fugit! Tempora neque minus tenetur vitae beatae enim consequuntur. Sit,
+                aspernatur?</p>
+            <p>Quasi voluptatem, pariatur, commodi odio ratione modi atque fuga iusto unde repellat sit ut eligendi.
+                Cumque neque autem maiores libero, perspiciatis odio doloremque et deleniti! Sunt reprehenderit odit
+                quidem aspernatur!</p>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,28 +14,7 @@
 
 <body>
     <div class="container">
-        <div class="bg-gray-400 p-4 h-64 overflow-y-scroll">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe accusantium laudantium esse. Molestiae
-                architecto qui possimus voluptatibus. Optio incidunt voluptas tenetur qui error. Fuga dolore sit at
-                porro dolorum recusandae?</p>
-            <p>Deleniti repellendus consequatur aut repudiandae tempora facilis nesciunt nobis debitis cupiditate soluta
-                voluptates rem minima ipsum, expedita consequuntur aliquid quibusdam numquam maxime illum excepturi
-                dolores. Reiciendis tenetur deserunt minus cupiditate.</p>
-            <p>Saepe consequatur repellendus repudiandae minus asperiores minima odio odit mollitia explicabo illo, ipsa
-                quod eum ad rem eos ullam possimus tempore, soluta deserunt itaque quisquam non eius? Ullam, molestias
-                recusandae.</p>
-            <p>Cupiditate repellat ipsam voluptates adipisci unde doloremque, deserunt consequatur officiis maiores
-                nostrum quis, iusto sed corrupti itaque totam, asperiores eum deleniti cum consequuntur enim? Neque cum
-                laborum asperiores debitis pariatur!</p>
-            <p>Doloremque ratione natus quod magnam explicabo eveniet minus repellat, minima doloribus similique tempora
-                libero esse non id soluta saepe. Ipsam nobis doloribus in aut cumque velit cum explicabo ad magni?</p>
-            <p>Odio dolorum eius sequi deserunt ad provident vero iste asperiores ratione sit omnis corrupti facere amet
-                delectus, cupiditate eos fugit! Tempora neque minus tenetur vitae beatae enim consequuntur. Sit,
-                aspernatur?</p>
-            <p>Quasi voluptatem, pariatur, commodi odio ratione modi atque fuga iusto unde repellat sit ut eligendi.
-                Cumque neque autem maiores libero, perspiciatis odio doloremque et deleniti! Sunt reprehenderit odit
-                quidem aspernatur!</p>
-        </div>
+
 
     </div>
 
