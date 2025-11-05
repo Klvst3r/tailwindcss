166070d4d59f5eba1dc68f9a66cb0c2388aea82a
Klvst3r <klvst3r@gmail.com>
2025-11-05 10:03:28 -0600

    Imagenes y texto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,12 +13,30 @@
 </head>
 
 <body>
-    <div class="container mt-4">
-        <div class="bg-gray-300 w-64 h-32 p-8 border-8 border-gray-500 box-content">
-            <div class="bg-gray-500 h-full w-full">
-                
-            </div>
-        </div>
+    <div class="container">
+
+        <img src="https://images.pexels.com/photos/34336341/pexels-photo-34336341.jpeg" alt="Night"
+            class="w-72 h-64 object-cover rounded-lg shadow-md float-left" />
+
+        <img src="https://images.pexels.com/photos/33430991/pexels-photo-33430991.jpeg" alt="Night"
+            class="w-72 h-64 object-cover rounded-lg shadow-md float-right" />
+
+        <p class="mb-2 clear-both">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur repellat cum,
+            qui
+            tempora odio incidunt vitae et illum quia nesciunt deserunt doloribus nemo eum aperiam eius atque ut.
+            Itaque, rerum.</p>
+        <p class="mb-2">Ut maiores eligendi saepe ipsa culpa nam, possimus vero incidunt quisquam sed illo minima,
+            explicabo, ad alias voluptatem ratione asperiores itaque quaerat nobis voluptatibus consectetur modi
+            officiis quibusdam voluptas? Modi.</p>
+        <p class="mb-2">Dignissimos quae consectetur excepturi quis adipisci veniam animi vitae eligendi libero
+            repellat similique quibusdam impedit ullam, praesentium magnam quos molestias omnis illum velit sint, in
+            eaque sunt expedita. Laborum, maxime.</p>
+        <p class="mb-2">Quasi excepturi saepe atque eum molestiae dolores repellat ipsa similique, facere commodi
+            dignissimos maxime? Exercitationem sunt autem nemo, impedit debitis unde ab laboriosam nisi praesentium
+            distinctio dignissimos accusantium quisquam earum.</p>
+        <p class="mb-2">Similique quasi illo inventore corrupti consectetur ab hic, quis, neque fugit repellendus
+            nesciunt voluptas velit, pariatur aliquid voluptatibus mollitia quas asperiores odio nihil eveniet eum
+            voluptatem porro necessitatibus! Facere, in!</p>
     </div>
 </body>
 
